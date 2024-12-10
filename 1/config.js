e63db5da5d591fe5e49b2e5a85fe92497f290a25
gossip777 <gossip777gossip@gmail.com>
2024-12-10 17:37:14 +0800

    Create config.js

---
+++ 1/config.js
@@ -0,0 +1,102 @@
+module.exports = {
+    proxyBase: {
+      protocol: 'socks5',
+      username: '',
+      password: '',
+      port: 1080,
+    },
+    proxyIPs: [
+      '185.153.150.13',
+      '91.148.241.5',
+      '91.148.237.38',
+      
+    ],
+  };
+
+syd.socks.privado.io
+45.38.18.180
+
+ams.socks.privado.io
+91.148.245.29
+
+ath.socks.privado.io
+91.148.237.38
+
+ber.socks.privado.io
+91.148.237.38
+
+bud.socks.privado.io
+91.148.237.38
+
+eze.socks.privado.io
+85.12.6.71
+
+fra.socks.privado.io
+91.148.237.38
+
+hkg.socks.privado.io
+91.148.237.38
+
+ist.socks.privado.io
+91.148.237.38
+
+jnb.socks.privado.io
+91.148.237.38
+
+iev.socks.privado.io
+91.148.237.38
+
+lhr.socks.privado.io
+91.148.245.5
+
+lax.socks.privado.io
+45.38.18.180
+
+man.socks.privado.io
+91.148.241.69
+
+mia.socks.privado.io
+45.38.17.227
+
+mxp.socks.privado.io
+mxp.socks.privado.io
+
+yul.socks.privado.io
+45.38.17.227
+
+bom.socks.privado.io
+91.148.237.38
+
+cdg.socks.privado.io
+91.148.241.69
+
+gru.socks.privado.io
+85.12.6.71
+
+sin.socks.privado.io
+91.148.237.38
+
+arn.socks.privado.io
+91.148.245.5
+
+syd.socks.privado.io
+45.38.18.180
+
+nrt.socks.privado.io
+45.38.18.180
+
+yyz.socks.privado.io
+85.12.61.9
+
+yvr.socks.privado.io
+45.38.18.180
+
+vie.socks.privado.io
+91.148.237.38
+
+dca.socks.privado.io
+45.38.17.227
+
+zrh.socks.privado.io
+91.148.237.38
+
